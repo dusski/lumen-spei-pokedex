3482990b5ba1395ad92d91a8be42d47b02e49613
dusski <dusski@gmail.com>
2022-12-17 15:45:18 +0100

    Clean up styles and pregenerated code

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.scss'
 import { Outlet } from "react-router-dom"
+import './App.scss'
+
+import Container from 'react-bootstrap/Container'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-      <div className="App">
-        <Outlet />
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://reactjs.org" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}!
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </div>
+      <main className="App">
+        <Container fluid>
+          <Outlet />
+        </Container>
+      </main>
   )
 }
 
 export default App
+
+// const [count, setCount] = useState(0)
+/* <div>
+  <a href="https://vitejs.dev" target="_blank">
+    <img src="/vite.svg" className="logo" alt="Vite logo" />
+  </a>
+  <a href="https://reactjs.org" target="_blank">
+    <img src={reactLogo} className="logo react" alt="React logo" />
+  </a>
+</div>
+<h1>Vite + React</h1>
+<div className="card">
+  <button className="btn btn-primary" onClick={() => setCount((count) => count + 1)}>
+    count is {count}!
+  </button>
+  <Button variant="primary" onClick={() => setCount((count) => count + 1)}>count is {count}!</Button>
+  <p>
+    Edit <code>src/App.tsx</code> and save to test HMR
+  </p>
+</div>
+<p className="read-the-docs">
+  Click on the Vite and React logos to learn more
+</p> */
