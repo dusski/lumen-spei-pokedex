f609665e397b1b67188d231e8d2e408e791a78fe
dusski <dusski@gmail.com>
2022-12-18 18:04:24 +0100

    Use auth context in homepage and header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,54 @@
-import "./Header.scss"
+import './Header.scss'
 
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { NavLink, useNavigate } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { useContext } from 'react'
+import { AuthContext } from '../context/Auth.context'
+import { Button } from 'react-bootstrap'
 
 function Header() {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
+  const {
+    user: { email, firstName },
+    setUser
+  } = useContext(AuthContext)
+  let isUserAuthenticated = !!email
+
+  const handleLogout = () => {
+    setUser({})
+    isUserAuthenticated = false;
+  }
 
   return (
     <>
       <Navbar bg="light">
-        <Navbar.Brand className="navbar-brand" onClick={() => navigate('/')}>React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand className="navbar-brand" onClick={() => navigate('/')}>
+          React-Bootstrap
+        </Navbar.Brand>
         <Nav className="me-auto">
-          <NavLink to="/" className={({isActive}) => isActive ? 'nav-link' : 'nav-link active'}>Home</NavLink>
-          <NavLink to="/register" className={({isActive}) => isActive ? 'nav-link' : 'nav-link active'}>Register</NavLink>
+          <NavLink to="/" className={({ isActive }) => (isActive ? 'nav-link' : 'nav-link active')}>
+            Home
+          </NavLink>
+          <NavLink
+            to="/register"
+            className={({ isActive }) => (isActive ? 'nav-link' : 'nav-link active')}>
+            Register
+          </NavLink>
         </Nav>
-    </Navbar>
+        {isUserAuthenticated && (
+          <Nav className="justify-content-end align-items-center">
+            <Navbar.Text>{firstName}</Navbar.Text>
+            <NavLink
+              to="/register"
+              className={({ isActive }) => (isActive ? 'nav-link' : 'nav-link active')}>
+              <Button variant="primary" onClick={() => handleLogout()}>
+                Logout
+              </Button>
+            </NavLink>
+          </Nav>
+        )}
+      </Navbar>
     </>
   )
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,37 @@
-import Header from '../components/Header';
+import { useContext, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Header from '../components/Header'
+
+import { AuthContext } from '../context/Auth.context'
 
 function HomePage() {
+  const navigate = useNavigate()
+  const { user } = useContext(AuthContext)
+
+  useEffect(() => {
+    if (!user.email) {
+      navigate('/register')
+      return
+    }
+  })
+
+
   return (
     <>
       <Header></Header>
-      <p>HomePage works!</p>
+      {/* <p>HomePage works!</p> */}
+      {/* <pre>{JSON.stringify(user, null, 2)}</pre> */}
     </>
   )
 }
 
 export default HomePage
+
+// {!!token ?
+//  :
+// <div>
+//   {/* <Button className="btn btn-link">ASd</Button> */}
+//   <Link className="btn btn-link" to={'/register'}>Register</Link>
+//   <Link className="btn btn-link" to={'/login'}>Login</Link>
+// </div>
+// }
