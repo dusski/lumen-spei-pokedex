69a68e79ca7e3ab0ef4781dd9e557f55794815e3
dusski <dusski@gmail.com>
2022-12-18 20:18:26 +0100

    Reformat main.tsx and add new route for pokemon page

--- src/main.tsx
+++ src/main.tsx
@@ -2,39 +2,43 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.scss'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import HomePage from './pages/HomePage';
-import ErrorPage from './pages/ErrorPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import ErrorPage from './pages/ErrorPage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import PokemonPage from './pages/PokemonPage';
+import { pokemonIdLoader } from './pages/PokemonPage';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App/>,
-    errorElement: <ErrorPage/>,
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
-        path: "",
-        element: <HomePage />,
+        path: '',
+        element: <HomePage />
       },
       {
-        path: "login",
-        element: <LoginPage />,
+        path: 'login',
+        element: <LoginPage />
       },
       {
-        path: "register",
-        element: <RegisterPage />,
+        path: 'register',
+        element: <RegisterPage />
       },
+      {
+        path: 'pokemon/:pokemonId',
+        element: <PokemonPage />,
+        loader: pokemonIdLoader
+      }
     ]
-  },
-]);
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
